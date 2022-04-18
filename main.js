7938411b15367344fe983c0275b6d47bbf5e5f46
murillonb <murillo_099@hotmail.com>
2022-04-18 20:33:17 -0300

    subindo aula aluramidi completo

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();    
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -12,4 +18,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {       
         tocaSom(idAudio);
     }
+
+     tecla.onkeydown = function(evento) {        
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+     }
+
+     tecla.onkeyup = function() {
+         tecla.classList.remove('ativa');
+     }
+
 } 
